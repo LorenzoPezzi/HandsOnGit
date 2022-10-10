e7faa2c50eaff5ff871c883d598b26b8e72dd413
Antonio <ceraoloanto@gmail.com>
2022-10-10 14:16:33 +0200

    completed method to get the average size of files inside a directory

--- HandsOnGit/Helper.cs
+++ HandsOnGit/Helper.cs
@@ -1,6 +1,29 @@
 ﻿namespace HandsOnGit
 {
-    internal class Helper
+    internal static class Helper
     {
+        public static string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        public static string FolderPath { get; private set; } = $"{currentDirectory}..\\..\\..\\Files";
+
+        public static double GetAVGFileSize()
+        {
+            List<double> dimensions = new();
+            DirectoryInfo dirInfo = new(FolderPath);
+
+
+
+            dirInfo.GetFiles().ToList().ForEach(fileInfo => dimensions.Add(fileInfo.Length));
+            return GetAverage(dimensions);
+        }
+        private static double GetAverage(List<double> values)
+        {
+            double somma = 0;
+
+            foreach (var value in values)
+            {
+                somma += value;
+            }
+            return somma / values.Count;
+        }
     }
 }
