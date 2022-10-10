1974616d8a19653e594694c74fa58ac935f1b795
Alessandro Rotili <a.rotili94@gmail.com>
2022-10-10 11:59:31 +0200

    Creazione cartella Model e classe FileManager

---
+++ HandsOnGit/Model/FileInfoCheck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnGit.Model
+{
+    public static class FileManager
+    {
+    }
+}
