27472c34e99e8dd7f4e468677d968fb418088c7d
Antonio <ceraoloanto@gmail.com>
2022-10-10 16:00:02 +0200

    ReAdded Read method in Helper

--- HandsOnGit/Helper.cs
+++ HandsOnGit/Helper.cs
@@ -26,6 +26,27 @@ internal static class Helper
         return somma / values.Count;
     }
 
+    public static string Read(string file)
+    {
+        String line;
+        string text = String.Empty;
+        try
+        {
+            using StreamReader sr = new StreamReader(file);
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                text += line;
+                line = sr.ReadLine();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        return text;
+    }
+
     public static bool FindSchoolFile()
     {
         var allFiles = Directory.GetFiles(FolderPath);
