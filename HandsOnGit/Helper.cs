6e71fa71ac1bd24ff911114aebd81d974928d7fd
Francesco Fera <98550815+Francesco-Fera@users.noreply.github.com>
2022-10-10 14:12:46 +0200

    Fix bug relative paths

--- HandsOnGit/Helper.cs
+++ HandsOnGit/Helper.cs
@@ -4,13 +4,14 @@
     {
 
         public static string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        public static string fullPath = Path.Combine(currentDirectory, @"..\..\..\Files");
+        public static string fullPath = System.IO.Path.Combine(currentDirectory, @"..\..\..\Files");
+        //public static string fullPathToSchool = Path.Combine(currentDirectory, @"..\..\..\Files\School.txt");
         public static int fileCount = Directory.GetFiles(fullPath).Length;
 
         public static bool FindSchoolFile()
         {
             var allFiles = Directory.GetFiles(fullPath);
-            string schoolFile = Path.Combine(fullPath, @"\School.txt");
+            string schoolFile = System.IO.Path.Combine(currentDirectory, "..\\..\\..\\Files\\School.txt");
             if (allFiles.Contains(schoolFile))
             {
                 return true;
@@ -20,7 +21,7 @@
 
         public static void WriteStudentFile(string json)
         {
-            string studentFilePath = "C:\\Users\\ilfer\\Desktop\\handson\\HandsOnGit\\Files\\Student.txt";
+            string studentFilePath = System.IO.Path.Combine(currentDirectory, "..\\..\\..\\Files\\Student.txt");
             using (StreamWriter sw = new StreamWriter(studentFilePath))
             {
                 sw.WriteLine(json);
