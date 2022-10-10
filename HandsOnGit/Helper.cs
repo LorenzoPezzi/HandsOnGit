a9020d3c87e2b0ef79c1431f15e07a9eb4b57dd1
Riccardo Stagni <riccardo.stagni2000@gmail.com>
2022-10-10 15:03:26 +0200

    Aaa

--- HandsOnGit/Helper.cs
+++ HandsOnGit/Helper.cs
@@ -1,13 +1,17 @@
 ﻿namespace HandsOnGit
 {
-    internal static class Helper
+    public static class Helper
     {
-        public static int fileCount = Directory.GetFiles("C:\\Users\\ilfer\\Desktop\\handson\\HandsOnGit\\Files\\").Length;
+
+        public static string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        public static string fullPath = System.IO.Path.Combine(currentDirectory, @"..\..\..\Files");
+        //public static string fullPathToSchool = Path.Combine(currentDirectory, @"..\..\..\Files\School.txt");
+        public static int fileCount = Directory.GetFiles(fullPath).Length;
 
         public static bool FindSchoolFile()
         {
-            var allFiles = Directory.GetFiles("C:\\Users\\ilfer\\Desktop\\handson\\HandsOnGit\\Files\\");
-            string schoolFile = "C:\\Users\\ilfer\\Desktop\\handson\\HandsOnGit\\Files\\School.txt";
+            var allFiles = Directory.GetFiles(fullPath);
+            string schoolFile = System.IO.Path.Combine(currentDirectory, "..\\..\\..\\Files\\School.txt");
             if (allFiles.Contains(schoolFile))
             {
                 return true;
@@ -17,7 +21,7 @@
 
         public static void WriteStudentFile(string json)
         {
-            string studentFilePath = "C:\\Users\\ilfer\\Desktop\\handson\\HandsOnGit\\Files\\Student.txt";
+            string studentFilePath = System.IO.Path.Combine(currentDirectory, "..\\..\\..\\Files\\Student.txt");
             using (StreamWriter sw = new StreamWriter(studentFilePath))
             {
                 sw.WriteLine(json);
@@ -26,4 +30,3 @@
         }
     }
 }
-
--- HandsOnGit/Student.cs
+++ HandsOnGit/Student.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HandsOnGit
 {
-    internal class Student
+    public class Student
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
